39c3c3581d38f7f2f83f978f039f2a0764dae95a
ADA-AbdusSamad <ada.abdussamad@gmail.com>
2024-04-19 15:36:17 +0530

    meta tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,10 +13,28 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
- 
+
   return (
     <html lang="en">
       <head>
+        <meta name="keywords" content="Elite Group" />
+        <meta name="description"
+          content="Your end to end amazon sales booster." />
+        <meta property="og:title" content="Elite Group" />
+        <meta property="og:description"
+          content="Where Every Bricks Tells a Tale." />
+        <meta property="og:image" itemprop="image"
+          content="https://dev.alphadigitall.com/image/logo-dark-512.png" />
+
+        <meta property="og:image:width" content="300" />
+        <meta property="og:image:height" content="300" />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:image:type" content="image/jpeg" />
+
+        <meta name="author" content="" />
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossOrigin="anonymous" />
         {/* <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-element-bundle.min.js"></script>
